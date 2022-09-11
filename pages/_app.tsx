fb79045021b844780a7db0f0dcc471afa2b28bca
Yanislav Igonin <yanislavdabest@gmail.com>
2022-09-11 09:23:39 +0300

    add session provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider as AuthProvider,  } from 'next-auth/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <AuthProvider session={session}>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
 
 export default MyApp
