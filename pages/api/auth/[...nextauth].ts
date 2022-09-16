6db900df59e21fda41b6a0a62dde67f444d202a2
Yanislav Igonin <yanislavdabest@gmail.com>
2022-09-16 16:08:41 +0300

    add confirm email template

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,41 @@
 import NextAuth from 'next-auth';
-import EmailProvider from 'next-auth/providers/email';
+import EmailProvider, { SendVerificationRequestParams } from 'next-auth/providers/email';
 import { Client as FaunaClient } from 'faunadb';
 import { FaunaAdapter } from '@next-auth/fauna-adapter';
+import Handlebars from 'handlebars';
+import nodemailer from 'nodemailer';
+import { readFileSync } from 'fs';
+import path from 'path';
 import { config } from '@/config';
 
 const client = new FaunaClient(config.fauna);
 
+const transporter = nodemailer.createTransport({
+  ...config.email.server,
+  secure: true,
+});
+
+const emailsDir = path.resolve(process.cwd(), 'emails');
+
+const sendVerificationRequest = ({ identifier, url }: SendVerificationRequestParams) => {
+  const emailFile = readFileSync(path.join(emailsDir, 'confirm-email.html'), {
+    encoding: 'utf8',
+  });
+  const emailTemplate = Handlebars.compile(emailFile);
+  transporter.sendMail({
+    from: `"⚡ Magic NextAuth" ${process.env.EMAIL_FROM}`,
+    to: identifier,
+    subject: 'Your sign-in link for Magic NextAuth',
+    html: emailTemplate({
+      signin_url: url,
+      email: identifier,
+    }),
+  });
+};
+
 export default NextAuth({
   debug: config.app.isProduction,
-  providers: [EmailProvider(config.email)],
+  providers: [EmailProvider({maxAge: config.email.maxAge, sendVerificationRequest})],
   adapter: FaunaAdapter(client),
   pages: {
     signIn: '/auth/signin',
